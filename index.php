95d712295f88a46fb799bf5fc313f652d3ddc4f5
Lêvy Alves <levy.pereiraA1532@gmail.com>
2023-08-25 14:14:54 -0300

    Criando modelo para projeto em php

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./assets/css/style.css">
+  </head>
+  <body>
+    <div class="Wrapper">
+      <div class="Wrapper__container">
+        Hello World
+      </div>
+    </div>
+
+    <script src="./assets/js/libs/jquery.min.js"></script>
+    <script src="./assets/js/main.min.js"></script>
+
+  </body>
+</html>
